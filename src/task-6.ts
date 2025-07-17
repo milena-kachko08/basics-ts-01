6075d86b63c9b9a07eac167f938936c718b0e096
Milena Kachko <kachkomilena@gmail.com>
2025-07-17 16:45:08 +0200

    Initial commit

---
+++ src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const num = getFirstElement<number>([1, 2, 3]);           // number
+const str = getFirstElement<string>(["a", "b", "c"]);     // string
+const bool = getFirstElement<boolean>([true, false]);     // boolean
+
+getFirstElement([1, "a", true]); 
